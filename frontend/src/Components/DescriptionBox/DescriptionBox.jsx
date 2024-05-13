472cced61b74cc209baf618e172c4616f8f1f3f5
MouBoni1 <mouboni2000@gmail.com>
2024-05-13 22:41:46 +0530

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox =()=>{
+return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">
+                Description
+            </div>
+            <div className="descriptionbox-nav-box fade">
+                Reviews(122)
+            </div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An ecommerce platform is the content management system (CMS) and commerce engine websites use to manage catalogued products, register purchases and manage a users relationship with an online retailer. It doesn't matter if your business is large or small, B2B or B2C, selling tangible goods or providing remote services.</p>
+            
+        </div>
+
+    </div>
+)
+
+}
+export default DescriptionBox;
